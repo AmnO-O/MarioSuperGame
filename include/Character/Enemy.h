98a520d565cba4c5a60916c843e058a99036474b
phongcp <nguyentronghungphong001@gmail.com>
2025-08-01 20:39:37 +0700

    Modify Enemy class

--- include/Character/Enemy.h
+++ include/Character/Enemy.h
@@ -1,17 +1,31 @@
 #pragma once
-#include "Character/Character.h"
+#include "Character.h"
 #include "Observer/ICollidable.h"
 
 class Enemy : public Character, public ICollidable {
 protected:
-    bool alive = true;
+    CharacterType type;
+    std::map<std::string, std::unique_ptr<AnimationManager>> animations;
+
+    void readRectAnimation(const std::string& filePath, Texture2D& sheet) ;
 
 public:
-    Enemy(Vector2 pos);
-    virtual ~Enemy() = default;
+    Enemy(CharacterType type, Vector2 pos);
 
-    Rectangle getHitbox() const override;
-    bool IsActive() const override;
-    void kill();
-    virtual void adaptCollision(ICollidable* other) override;
+    virtual void updateAnimationType() = 0;
+    void update(float deltaTime) override;
+    void render() override;
+
+    Rectangle getHitbox() const override { return hitbox; }
+    Vector2 getPosition() const { return position; }
+
+    void setPosition(const Vector2& pos);
+    void setOnGround();
+
+    void setDead();
+    bool isDead() const { return dead; }
+
+    void adaptCollision(ICollidable* other) override;
+
+    ~Enemy() {}
 };
