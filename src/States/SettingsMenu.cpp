81e7aa490309873614c66e2a593c250e514c71b3
NotSqrt77 <buicongvinh07072006@gmail.com>
2025-08-01 21:09:43 +0700

    Fix directory issues (final)

--- src/States/SettingsMenu.cpp
+++ src/States/SettingsMenu.cpp
@@ -7,16 +7,16 @@ SettingsMenu::SettingsMenu(StateManager& stateManager, SoundManager& soundManage
   : stateManager(stateManager),
     soundManager(soundManager),
     slider({686, 245, 449, 35}, soundManager),
-    return_button("assets/images/turn_back.png", {25, 27, 100, 100}, [&]() {
+    return_button("../assets/images/turn_back.png", {25, 27, 100, 100}, [&]() {
         //stateManager.popState();
         stateManager.pushState(std::make_unique<MainMenu>(stateManager, soundManager));
     }),
     music("MUSIC:"),
     soundFX("SOUND FX:") 
 {    
-    settings_background = LoadTexture("assets/images/blue_background.png");
-    return_button_state = LoadTexture("assets/images/turn_back_red.png");
-    font = LoadFont("assets/fonts/SuperMarioBros.ttf");
+    settings_background = LoadTexture("../assets/images/blue_background.png");
+    return_button_state = LoadTexture("../assets/images/turn_back_red.png");
+    font = LoadFont("../assets/fonts/SuperMarioBros.ttf");
 }
 
 SettingsMenu::~SettingsMenu()
