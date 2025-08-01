153d3983b2c0ee97ee8756b40c39bb1688f2c976
phongcp <nguyentronghungphong001@gmail.com>
2025-08-01 20:58:07 +0700

    Creating Koopa class

---
+++ include/Character/Koopa.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Enemy.h"
+
+class Koopa : public Enemy {
+public:
+    Koopa(Vector2 pos) : Enemy(CharacterType::KOOPA, pos) {
+        velocity.x = -35.0f;
+        readRectAnimation("assets/animation/koopa.txt", Images::textures["enemies1.png"]);
+    }
+
+    void update(float deltaTime) override;
+    void updateAnimationType() override;
+    void adaptCollision(ICollidable* other) override;
+};
