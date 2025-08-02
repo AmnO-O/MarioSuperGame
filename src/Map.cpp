e63fa6b0f6f88118f12b2d2cd3adb280af9e30f6
phongcp <nguyentronghungphong001@gmail.com>
2025-08-02 22:26:37 +0700

    Remove Colision when enemy dead

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,6 +1,7 @@
 #include "Map.h"
 #include "Blocks/Brick.h"
 #include "Blocks/Floor.h"
+#include "Observer/Observer.h"
 #include <fstream>
 
 void Map::input(std::istream &is, Texture objectTex) {
@@ -52,12 +53,21 @@ Map::Map(const std::string& folderPath, Texture objectTex) {
     fin.close();
 }
 
-void Map::Update(float delta) {
+void Map::Update(float delta, CollisionManager &cm) {
     for (int i = 0; i < blocks.size(); i++)
         blocks[i]->Update(delta);
 
-    for (int i = 0; i < enemies.size(); i++) {
+    // Update enemies and remove dead ones
+    for (int i = enemies.size() - 1; i >= 0; i--) {
         enemies[i]->update(delta);
+        
+        // Remove enemy if dead
+        if (enemies[i]->isDead()) {
+            // Unregister from collision manager before deleting
+            cm.Unregister(enemies[i]);
+            delete enemies[i];
+            enemies.erase(enemies.begin() + i);
+        }
     }
 }
 
