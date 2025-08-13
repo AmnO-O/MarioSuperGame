a14e37d7065774e003e1391d6bfdd2de1e3757bf
wzyle <lequocvi2412@gmail.com>
2025-08-13 22:57:01 +0700

    Fix Piranha

--- src/Piranha.cpp
+++ src/Piranha.cpp
@@ -1,11 +1,24 @@
 #include "Character/Piranha.h"
 #include "Character/Character.h"
 
-void Piranha::update(float deltaTime) {
-    Enemy::update(deltaTime);
+void Piranha::update(float deltaTime) {    if (dead) return;
+    if (state == State::DIE2) {
+        velocity = {20.0f, -75.0f};
+        delayDead += deltaTime;
+        if (delayDead >= 0.2f) setDead2();
+    }
+    
+    position.x += velocity.x * deltaTime;
+    position.y += velocity.y * deltaTime;
+
+    if (activeAnimation) {
+        activeAnimation->update(deltaTime);
+        updateHitbox();
+    }
+
     if (isGoing) {
         timer += deltaTime;
-        if (timer >= 2.0f) {
+        if (timer >= 3.0f) {
             velocity.y = -velocity.y;
             timer = 0.0f;
         }   
